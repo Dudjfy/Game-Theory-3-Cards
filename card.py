7a580148a98fb76e10a03a1f198925dc92261e58
Dmitry Chirin <chirin.dima@gmail.com>
2022-05-21 16:22:01 +0200

    remade the card class to be a data class, seems to work just as fast

--- card.py
+++ card.py
@@ -1,10 +1,16 @@
+"""Card class module"""
+from dataclasses import dataclass
+
+
+@dataclass
 class Card:
-    def __init__(self, value: int):
-        self.value = value
-        self.face_up = True
+    """Card class, inspired by black jack done a while ago"""
+    value: int
+    face_up: bool = True
 
     def __str__(self):
         return f"[{self.value:}]" if self.face_up else "[■]"
 
     def flip_face(self):
+        """Flips to opposite face, mainly unused now"""
         self.face_up = not self.face_up
